aa6839932eb829ea672b7bca6948aeecee46638d
CadinhoDev <cadinhodev@gmail.com>
2024-12-17 08:48:37 -0300

    initial_files

---
+++ src/createWallet.js
@@ -0,0 +1,8 @@
+// Importando as dependências
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
+
+// Definir Rede
+const network = bitcoin.networks.testnet;
+
